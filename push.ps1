4081b9bfffffc8aa2cf22c7b205bf5c36e4885da
Shred-der <teminimighty@gmail.com>
2026-07-12 05:15:18 +0100

    chore: remove scratch files, build artifacts, and dev-time scripts

--- push.ps1
+++
@@ -1,4 +0,0 @@
-gh repo create CeeBanks-Portfolio-V2 --public 2> error.txt > out.txt
-git branch -M main
-git remote add origin https://github.com/tryMighty/CeeBanks-Portfolio-V2.git
-git push -u origin main 2>> error.txt >> out.txt
