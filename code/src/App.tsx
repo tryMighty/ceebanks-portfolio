13a0fffd7d788bd82c166a2d0f99d4f1b347e335
Shred-der <teminimighty@gmail.com>
2026-07-12 02:45:56 +0100

    chore: revert image z-index to place it behind text

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -230,7 +230,7 @@ export default function App() {
           </div>
         </div>
 
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full max-w-5xl h-[72vh] md:h-[77vh] flex justify-center items-end z-[110] pointer-events-none">
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full max-w-5xl h-[72vh] md:h-[77vh] flex justify-center items-end z-20 pointer-events-none">
           <img ref={imageRef} src={ceebanksImg} alt="CeeBanks"
             className="h-full w-auto object-contain object-bottom drop-shadow-[0_20px_40px_rgba(0,0,0,0.35)] scale-[1.15] md:scale-[1.25] origin-bottom" />
         </div>
